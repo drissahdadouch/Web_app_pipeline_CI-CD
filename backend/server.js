94e0ec68ba513f8a11b9107e0956406974172e2e
drissahdadouch <drissahd@gmail.com>
2025-03-06 22:15:52 +0000

    pushing Dockerfiles

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
 
+// Configure Cloudinary with environment variables
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
   api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running at port ${process.env.PORT}`);
+// Set the port to either the environment variable or default to 5000
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
 });
+
